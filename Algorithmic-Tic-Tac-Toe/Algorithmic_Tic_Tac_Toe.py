8c63d3d0d877f9bc7d143b3f0a32c7f155161d28
David Liang <davliang.stu@gmail.com>
2020-08-31 02:13:07 -0500

    Init Game Loop

--- Algorithmic-Tic-Tac-Toe/Algorithmic_Tic_Tac_Toe.py
+++ Algorithmic-Tic-Tac-Toe/Algorithmic_Tic_Tac_Toe.py
@@ -1,1 +1,14 @@
+import pygame
 
+pygame.init()
+
+_WIDTH = 800
+_HEIGHT = 600
+
+_DISPLAY = pygame.display.set_mode((_WIDTH, _HEIGHT))
+
+game_end = False
+
+while not game_end:
+    for event in pygame.event.get():
+        print(event)
