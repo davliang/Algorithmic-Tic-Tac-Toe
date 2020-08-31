a1205d0c634543d96ac243b937a348e438eedaa0
David Liang <davliang.stu@gmail.com>
2020-08-31 03:06:36 -0500

    Debug Draw Working

--- Algorithmic-Tic-Tac-Toe/Algorithmic_Tic_Tac_Toe.py
+++ Algorithmic-Tic-Tac-Toe/Algorithmic_Tic_Tac_Toe.py
@@ -25,10 +25,33 @@ pygame.draw.line(background, (0, 0, 0), ((2/3) * _WIDTH, 25), ((2/3) * _WIDTH, _
 pygame.draw.line(background, (0, 0, 0), (25, (1/3) * _HEIGHT), (_WIDTH - 25, (1/3) * _HEIGHT), 2)
 pygame.draw.line(background, (0, 0, 0), (25, (2/3) * _HEIGHT), (_WIDTH - 25, (2/3) * _HEIGHT), 2)
 
+# Update screen with background
+screen.blit(background, (0, 0))
+
+# Import and rescale images
+scale_img_x = int(_WIDTH / 4)
+scale_img_y = int(_HEIGHT / 4)
+
+x_move = pygame.image.load("img/x.png")
+x_move = pygame.transform.scale(x_move, (scale_img_x , scale_img_y))
+o_move = pygame.image.load("img/o.png")
+o_move = pygame.transform.scale(o_move, (scale_img_x , scale_img_y))
+
+# Check board for winner
 def check_board():
     global board
 
-screen.blit(background, (0, 0))
+# Debug function to test mouse drawing
+move = False
+def test_draw():
+    global move
+    x, y = pygame.mouse.get_pos()
+    if move:
+        screen.blit(o_move, (x - scale_img_x / 2, y - scale_img_y / 2))
+    else:
+        screen.blit(x_move, (x - scale_img_x / 2, y - scale_img_y / 2))
+    move = not move
+
 
 game_end = False
 
@@ -39,9 +62,13 @@ while not game_end:
         if event.type == pygame.QUIT:
             sys.exit(0)
 
+        # If event is mouseclickup
+        elif event.type == 6:
+            test_draw()
+
         pygame.display.flip()
 
-        print(event)
+        print(event.type)
 
     
     pygame.display.update()
