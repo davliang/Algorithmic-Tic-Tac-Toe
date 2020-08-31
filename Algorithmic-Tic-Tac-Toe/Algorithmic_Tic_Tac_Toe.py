f7f99798114523eb397889b2684455505b6cf430
David Liang <davliang.stu@gmail.com>
2020-08-31 03:33:41 -0500

    Removed test_draw() and randomized board input to test draw locations

--- Algorithmic-Tic-Tac-Toe/Algorithmic_Tic_Tac_Toe.py
+++ Algorithmic-Tic-Tac-Toe/Algorithmic_Tic_Tac_Toe.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+from random import randint
 
 pygame.init()
 
@@ -12,16 +13,16 @@ pygame.display.set_caption("Tic Tac Toe")
 # Import and rescale images
 scale_img_x = int(_WIDTH / 4)
 scale_img_y = int(_HEIGHT / 4)
-
-x_move = pygame.image.load("img/x.png")
-x_move = pygame.transform.scale(x_move, (scale_img_x , scale_img_y))
-o_move = pygame.image.load("img/o.png")
-o_move = pygame.transform.scale(o_move, (scale_img_x , scale_img_y))
-
-board = [[None] * 3, [None] * 3, [None] * 3]
+x_move = pygame.transform.scale(pygame.image.load("img/x.png"), (scale_img_x , scale_img_y))
+o_move = pygame.transform.scale(pygame.image.load("img/o.png"), (scale_img_x , scale_img_y))
 
 # Initialize game board to blank
 def init_board():
+
+    # Reset game board
+    global board
+    board = [[None] * 3, [None] * 3, [None] * 3]
+
     # Set background color to white
     background = pygame.Surface(screen.get_size())
     background = background.convert()
@@ -48,31 +49,22 @@ def check_board():
 
 # Draw current state of the board
 def draw_board():
+    global board
 
     init_board()
 
-    global board
+    board[randint(0,2)][randint(0,2)] = True
+    board[randint(0,2)][randint(0,2)] = False
 
     for x_index in range(0, 3):
             for y_index in range(0, 3):
                 if board[x_index][y_index] is not None:
+                    x = ((1/6) + (2/6) * x_index) * _WIDTH
+                    y = ((1/6) + (2/6) * y_index) * _HEIGHT
                     if board[x_index][y_index]:
                         screen.blit(x_move, (x - scale_img_x / 2, y - scale_img_y / 2))
                     else:
                         screen.blit(o_move, (x - scale_img_x / 2, y - scale_img_y / 2))
-
-
-# Debug function to test mouse drawing
-move = False
-def test_draw():
-    global move
-    x, y = pygame.mouse.get_pos()
-    if move:
-        screen.blit(o_move, ((3/6) * _WIDTH - scale_img_x / 2, (1/6) * _HEIGHT - scale_img_y / 2))
-    else:
-        screen.blit(x_move, ((3/6) * _WIDTH - scale_img_x / 2, (1/6) *  _HEIGHT - scale_img_y / 2))
-    move = not move
-
 
 game_end = False
 
@@ -85,7 +77,7 @@ while not game_end:
 
         # If event is mouseclickup
         elif event.type == 6:
-            test_draw()
+            draw_board()
 
         pygame.display.flip()
 
