d8b9b3663153f2732a4589a311ea3c0f1edcb1b3
David Liang <davliang.stu@gmail.com>
2020-08-31 02:32:34 -0500

    Game Background and Border Added

--- Algorithmic-Tic-Tac-Toe/Algorithmic_Tic_Tac_Toe.py
+++ Algorithmic-Tic-Tac-Toe/Algorithmic_Tic_Tac_Toe.py
@@ -1,14 +1,34 @@
 import pygame
+import sys
 
 pygame.init()
 
 _WIDTH = 800
 _HEIGHT = 600
 
-_DISPLAY = pygame.display.set_mode((_WIDTH, _HEIGHT))
+screen = pygame.display.set_mode((_WIDTH, _HEIGHT))
+pygame.display.set_caption("Tic Tac Toe")
+
+background = pygame.Surface(screen.get_size())
+background = background.convert()
+background.fill((255, 255, 255))
+
+border = pygame.draw.rect(background, (0, 0, 0), (0, 0, _WIDTH, _HEIGHT), 5)
+
+screen.blit(background, (0, 0))
 
 game_end = False
 
 while not game_end:
+
     for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            sys.exit(0)
+
+        pygame.display.flip()
+
         print(event)
+
+    
+    pygame.display.update()
